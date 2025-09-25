7dfe8e76e93fe4f4462337d2377d007ad7eaa083
Paradox098 <159344083+paradox974333@users.noreply.github.com>
2025-09-25 13:11:07 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,12 +17,24 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.80,
     },
     {
+      url: `${baseUrl}/blogs/rice-straw-vs-paper-straw-comparison`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.64,
+    },
+    {
+      url: `${baseUrl}/blogs/what-is-drinking-rice-straw-eco-friendly`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.64,
+    },
+    {
       url: `${baseUrl}/blogs/WhereToByRiceStrawsPostContent`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.64,
     },
-     {
+    {
       url: `${baseUrl}/blogs/order-eco-friendly-straws-online`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
